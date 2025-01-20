95f48cf5a83082e86985124573a0eaed1fbbb22a
DaviGreco <davigreco21@gmail.com>
2025-01-20 16:20:46 -0300

    Update overview.py

--- dashboard/overview.py
+++ dashboard/overview.py
@@ -68,21 +68,115 @@ def create_card(content, background_color):
         unsafe_allow_html=True,
     )
 
-def create_pie_chart(sentiment_counts):
-    labels, values = list(sentiment_counts.keys()), list(sentiment_counts.values())
-    fig = px.pie(
-        values=values,
-        names=labels,
-        color=labels, 
-        color_discrete_map={"Negativo": colors_labels[-1], "Positivo": colors_labels[1]},
-    )
-    fig.update_traces(textinfo="percent")
+def create_percentage_bar_chart(positives, negatives, reverse_colors=False):
+    total = positives + negatives
+    positive_rate = (positives / total) * 100
+    negative_rate = (negatives / total) * 100
+
+    fig = go.Figure()
+
+    if reverse_colors:
+        # Adiciona a barra de positivos (à direita)
+        fig.add_trace(go.Bar(
+            y=["Comentários"],
+            x=[positive_rate],
+            orientation='h',
+            marker=dict(color=colors_labels[1]),
+            name="Positivo",
+            text=f"{positive_rate:.2f}%",
+            textposition='inside'
+        ))
+
+        # Adiciona a barra de negativos (à esquerda)
+        fig.add_trace(go.Bar(
+            y=["Comentários"],
+            x=[negative_rate],
+            orientation='h',
+            marker=dict(color=colors_labels[-1]),
+            name="Negativo",
+            text=f"{negative_rate:.2f}%",
+            textposition='inside'
+        ))
+    else:
+        # Adiciona a barra de negativos (à esquerda)
+        fig.add_trace(go.Bar(
+            y=["Comentários"],
+            x=[negative_rate],
+            orientation='h',
+            marker=dict(color=colors_labels[-1]),
+            name="Negativo",
+            text=f"{negative_rate:.2f}%",
+            textposition='inside'
+        ))
+
+        # Adiciona a barra de positivos (à direita)
+        fig.add_trace(go.Bar(
+            y=["Comentários"],
+            x=[positive_rate],
+            orientation='h',
+            marker=dict(color=colors_labels[1]),
+            name="Positivo",
+            text=f"{positive_rate:.2f}%",
+            textposition='inside'
+        ))
+
     fig.update_layout(
-        height=200,  
-        width=200,   
-        margin=dict(t=25, b=10, l=10, r=10)  
+        barmode='stack',
+        xaxis=dict(
+            title="Porcentagem (%)",
+            range=[0, 100],
+            ticksuffix='%'
+        ),
+        yaxis=dict(
+            title="",
+            tickfont=dict(size=14)
+        ),
+        plot_bgcolor="white",
+        title="Porcentagem de Comentários Positivos e Negativos",
+        height=150,  # Aumenta a espessura da barra
+        margin=dict(l=10, r=10, t=30, b=10),
+        showlegend=False
     )
+
     return fig
+
+def render_topic_words(topic_number):
+    try:
+        with open('topic_modeling/data_topic_modeling/topics_kmeans2.json', 'r') as file:
+            topics_model = json.load(file)
+    except Exception as e:
+        st.error(f"Erro ao carregar os dados dos tópicos: {e}")
+        return
+
+    words_importance = topics_model.get(str(topic_number), [])
+    valid_words_importance = [
+        item for item in words_importance if isinstance(item, list) and len(item) == 2
+    ]
+    if len(valid_words_importance) == 0:
+        st.warning(f"Tópico {topic_number} não possui palavras com relevância.")
+        return
+
+    valid_words_importance = sorted(valid_words_importance, key=lambda x: x[1], reverse=True)
+    labels, values = zip(*valid_words_importance)
+
+    fig = go.Figure(go.Bar(
+        y=list(reversed(labels)),
+        x=list(reversed(values)),
+        orientation='h',
+        marker=dict(color='#3BCBDE')
+    ))
+
+    fig.update_layout(
+        title=f"Tópico {int(topic_number) + 1}: Relevância das Palavras",
+        xaxis_title="Relevância",
+        yaxis_title="Palavras",
+        plot_bgcolor="white",
+        height=300,
+        margin=dict(l=10, r=10, t=30, b=10) 
+    )
+
+    st.plotly_chart(fig, use_container_width=True, config={"staticPlot": True})
+
 
 def render_overview_topics():
     st.markdown("### Análise Geral dos Tópicos")
@@ -95,47 +189,52 @@ def render_overview_topics():
         st.error(f"Erro ao carregar os dados dos tópicos: {e}")
         return
 
-    # Iterar sobre cada tópico
-    for topic_number, words_importance in topics_model.items():
-        # Verificar se os dados do tópico estão no formato correto
-        if not isinstance(words_importance, list) or len(words_importance) == 0:
-            st.warning(f"Tópico {topic_number} não contém dados válidos.")
-            continue
-
-        # Ordenar palavras por relevância
-        valid_words_importance = [
-            item for item in words_importance if isinstance(item, list) and len(item) == 2
-        ]
-        if len(valid_words_importance) == 0:
-            st.warning(f"Tópico {topic_number} não possui palavras com relevância.")
-            continue
-
-        labels, values = zip(*valid_words_importance)
+    topic_numbers = list(topics_model.keys())
+    
+    # Iterar pelos tópicos em pares
+    for i in range(0, len(topic_numbers), 2):
+        col1, col2 = st.columns(2, gap="medium")
         
-        # Criar gráfico de barras horizontal
-        fig = go.Figure(go.Bar(
-            y=list(labels),
-            x=list(values),
-            orientation='h',
-            marker=dict(color='#3BCBDE')
-        ))
+        for col, topic_number in zip([col1, col2], topic_numbers[i:i+2]):
+            with col:
+                words_importance = topics_model[topic_number]
+                
+                # Verificar se os dados do tópico estão no formato correto
+                valid_words_importance = [
+                    item for item in words_importance if isinstance(item, list) and len(item) == 2
+                ]
+                if len(valid_words_importance) == 0:
+                    st.warning(f"Tópico {topic_number} não possui palavras com relevância.")
+                    continue
+                
+                # Ordenar palavras por relevância
+                valid_words_importance = sorted(valid_words_importance, key=lambda x: x[1], reverse=True)
+                labels, values = zip(*valid_words_importance)
+                
+                # Criar gráfico de barras horizontal
+                fig = go.Figure(go.Bar(
+                    y=list(reversed(labels)),  # Reverter para que a mais relevante fique no topo
+                    x=list(reversed(values)),
+                    orientation='h',
+                    marker=dict(color='#3BCBDE')  # Azul pastel escuro
+                ))
 
-        fig.update_layout(
-            title=f"Tópico {int(topic_number) + 1}: Relevância das Palavras",
-            xaxis_title="Relevância",
-            yaxis_title="Palavras",
-            plot_bgcolor="white"
-        )
+                fig.update_layout(
+                    title=f"Tópico {int(topic_number) + 1}: Relevância das Palavras",
+                    xaxis_title="Relevância",
+                    yaxis_title="Palavras",
+                    plot_bgcolor="white",
+                    height=400
+                )
 
-        # Exibir o gráfico
-        st.plotly_chart(fig, use_container_width=True)
+                # Exibir o gráfico
+                st.plotly_chart(fig, use_container_width=True)
 
 
 def render_positive_analysis(max, most_positive_topic, positives, negatives):
-    
-    best_question = max[0][2:].strip() 
+    best_question = max[0][2:].strip()
+    best_score = max[1]
 
-    best_score = max[1]  
     st.markdown("##### Pergunta com a Melhor Nota")
     create_card_with_score(
         question=best_question,
@@ -149,26 +248,27 @@ def render_positive_analysis(max, most_positive_topic, positives, negatives):
     with col1:
         st.markdown("###### Tópico com Mais Comentários Positivos")
         create_card(
-            content=f"Tópico {most_positive_topic+1}",
+            content=f"Tópico {most_positive_topic + 1}",
             background_color="#86E886"
         )
 
-        st.markdown(f"###### Resumo dos Comentários do Tópico {most_positive_topic+1}")
+        st.markdown(f"###### Resumo dos Comentários do Tópico {most_positive_topic + 1}")
         create_card(
             content=f"{positive_summary}",
             background_color="#86E886"
         )
 
     with col2:
-        sentiment_counts = {"Negativo": negatives, "Positivo": positives}
-        fig = create_pie_chart(sentiment_counts)
-        st.markdown(f"###### Taxa de Comentários Positivos e Negativos do Tópico {most_positive_topic+1}", unsafe_allow_html=True)
-        st.plotly_chart(fig, use_container_width=True, height=400)
+        fig = create_percentage_bar_chart(positives, negatives, reverse_colors=True)
+        st.plotly_chart(fig, use_container_width=True, config={"staticPlot": True})
+
+        # Adicionando gráfico de palavras
+        render_topic_words(most_positive_topic)
 
 def render_negative_analysis(min, most_negative_topic, positives, negatives):
-    
-    worst_question = min[0][1:].strip()  
-    worst_score = min[1]  
+    worst_question = min[0][1:].strip()
+    worst_score = min[1]
+
     st.markdown("##### Pergunta com a Pior Nota")
     create_card_with_score(
         question=worst_question,
@@ -177,26 +277,27 @@ def render_negative_analysis(min, most_negative_topic, positives, negatives):
     )
 
     col1, col2 = st.columns(2, gap="large")
+    negative_summary = load_topic_summary(f'data/overview_data/negativesummary.txt')
 
     with col1:
         st.markdown("###### Tópico com Mais Comentários Negativos")
         create_card(
-            content=f"Tópico {most_negative_topic+1}",
+            content=f"Tópico {most_negative_topic + 1}",
             background_color="#FFA6B1"
         )
 
-        negative_summary = load_topic_summary(f'data/overview_data/negativesummary.txt')
-        st.markdown(f"###### Resumo dos Comentários do Tópico {most_negative_topic+1}")
+        st.markdown(f"###### Resumo dos Comentários do Tópico {most_negative_topic + 1}")
         create_card(
             content=f"{negative_summary}",
             background_color="#FFA6B1"
         )
 
     with col2:
-        sentiment_counts = {"Negativo": negatives, "Positivo": positives}
-        fig = create_pie_chart(sentiment_counts)
-        st.markdown(f"###### Taxa de Comentários Positivos e Negativos do Tópico {most_negative_topic+1}", unsafe_allow_html=True)
-        st.plotly_chart(fig, use_container_width=True, height=400)
+        fig = create_percentage_bar_chart(positives, negatives)
+        st.plotly_chart(fig, use_container_width=True, config={"staticPlot": True})
+
+        # Adicionando gráfico de palavras
+        render_topic_words(most_negative_topic)
 
 def render_overview(df):
     means, max, min = calculate_means(df)
